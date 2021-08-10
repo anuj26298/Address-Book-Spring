95645405de8759ecbeea4db68ffa1f97b2546bae
anuj26298 <kumar26298anuj@gmail.com>
2021-08-10 21:32:25 +0530

    ADDED data anotation for getter and setter

--- src/main/java/com/example/addressbookspring/dto/ContactDTO.java
+++ src/main/java/com/example/addressbookspring/dto/ContactDTO.java
@@ -1,6 +1,9 @@
 package com.example.addressbookspring.dto;
 
-public class ContactDTO {
+import lombok.Data;
+
+public @Data
+class ContactDTO {
 
     private String name;
     private String city;
@@ -10,38 +13,5 @@ public class ContactDTO {
         this.name = name;
         this.city = city;
         this.state = state;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    @Override
-    public String toString() {
-        return "ContactDTO{" +
-                "name='" + name + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                '}';
     }
 }
